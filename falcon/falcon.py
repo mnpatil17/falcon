d350dab5fded10e6ce27276a74fb955a0c9e3ef7
Mihir Patil <mihir.patil@berkeley.edu>
2016-06-12 23:24:57 -0700

    Now we have all the c files in once place

--- falcon/falcon.py
+++ falcon/falcon.py
@@ -7,25 +7,6 @@ import numpy as np
 from elem_wise_arr_arr_op import specialize_arr_arr_element_wise
 from elem_wise_arr_scalar_op import specialize_arr_scalar_element_wise
 
-
-# Syntax
-
-def dcRem(input):  # input has to be one of our objects
-    avg = sum(input) / len(input)
-    return input - avg
-
-
-def specialize(func):
-    """
-    Enables the specialization of a function.
-
-    :param: func The function to decorate
-    """
-    builder = CFileBuilder()  # the builder that builds things on the first call (JIT)
-    # ... and continuing ...?
-    return func  # <-- TODO actually do a c_func...
-
-
 # TODO: We have to do more than just string together CFile objects, we have to actually compose
 class CFileBuilder:
     """
@@ -34,13 +15,13 @@ class CFileBuilder:
 
     def __init__(self):
         self._build_complete = False
-        self._c_file = CFile()          # TODO: actually make an empty CFile object here...
+        self._c_files = []
 
-    def add(self, c_function):          # Is this really supposed to be called "c_function?"
+    def add(self, new_c_files):          # Is this really supposed to be called "c_function?"
         """
         Adds a function to the C file.
         """
-        raise NotImplementedError
+        self._c_files.extend(new_c_files)
 
     def build(self):
         """
@@ -49,13 +30,14 @@ class CFileBuilder:
         :return: A CFile object that contains the desired code
         """
         self._build_complete = True
-        return self._c_file
+        return self._c_files
 
 
 class Scalar:
     """
     An implementation of SEJITS operations on scalar values.
     """
+
     def __init__(self, value):
         self.value = value
 
@@ -92,9 +74,18 @@ class FalconArray(np.ndarray):
             assert self.shape == other.shape, \
                 "Illegal element-wise multiplication with FalconArrays of shape {0} and {1}" \
                 .format(self.shape, other.shape)
-            return FalconArray.add_arr_arr_elem_wise(self, other)
+
+            result = FalconArray.array(FalconArray.add_arr_arr_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.add_arr_arr_elem_wise.latest_transform_result)
+
         else:
-            return FalconArray.add_arr_scalar_elem_wise(self, other)
+            result = FalconArray.array(FalconArray.add_arr_scalar_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.add_arr_scalar_elem_wise.latest_transform_result)
+        return result
 
     @staticmethod
     @specialize_arr_arr_element_wise
@@ -127,9 +118,17 @@ class FalconArray(np.ndarray):
             assert self.shape == other.shape, \
                 "Illegal element-wise multiplication with FalconArrays of shape {0} and {1}" \
                 .format(self.shape, other.shape)
-            return FalconArray.sub_arr_arr_elem_wise(self, other)
+
+            result = FalconArray.array(FalconArray.sub_arr_arr_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.sub_arr_arr_elem_wise.latest_transform_result)
         else:
-            return FalconArray.sub_arr_scalar_elem_wise(self, other)
+            result = FalconArray.array(FalconArray.sub_arr_scalar_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.sub_arr_scalar_elem_wise.latest_transform_result)
+        return result
 
     @staticmethod
     @specialize_arr_arr_element_wise
@@ -163,9 +162,20 @@ class FalconArray(np.ndarray):
                 "Illegal element-wise multiplication with FalconArrays of shape {0} and {1}" \
                 .format(self.shape, other.shape)
 
-            return FalconArray.mul_arr_arr_elem_wise(self, other)
+            result = FalconArray.array(FalconArray.mul_arr_arr_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.mul_arr_arr_elem_wise.latest_transform_result)
         else:
-            return FalconArray.mul_arr_scalar_elem_wise(self, other)
+            result = FalconArray.array(FalconArray.mul_arr_scalar_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.mul_arr_scalar_elem_wise.latest_transform_result)
+        return result
+
+    __rmul__ = __mul__
+    __radd__ = __add__
+    # TODO: __rsub__ and __rdiv__
 
     @staticmethod
     @specialize_arr_arr_element_wise
@@ -199,9 +209,17 @@ class FalconArray(np.ndarray):
                 "Illegal element-wise division with FalconArrays of shape {0} and {1}" \
                 .format(self.shape, other.shape)
 
-            return FalconArray.div_arr_arr_elem_wise(self, other)
+            result = FalconArray.array(FalconArray.div_arr_arr_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.div_arr_arr_elem_wise.latest_transform_result)
         else:
-            return FalconArray.div_arr_scalar_elem_wise(self, other)
+            result = FalconArray.array(FalconArray.div_arr_scalar_elem_wise(self, other))
+            result._add_to_c_files_for_array = self._add_to_c_files_for_array
+            self._add_to_c_files_for_array(
+                FalconArray.div_arr_scalar_elem_wise.latest_transform_result)
+
+        return result
 
     @staticmethod
     @specialize_arr_arr_element_wise
@@ -231,6 +249,19 @@ class FalconArray(np.ndarray):
 
     def __pow__(self, other):
         raise NotImplementedError
+
+    #
+    # Internal Methods
+    #
+
+    def _add_to_c_files_for_array(self, file_list):
+        """
+        This method should be re-assigned at some point before this FalconArray is used in
+        operations.
+
+        :param file_list: The list of files to add to the list of C files for the FalconArray
+        """
+        raise NotImplementedError()
 
     #
     # Creation Methods
@@ -281,6 +312,54 @@ class FalconArray(np.ndarray):
         return np.fromstring(*args, **kwargs).view(FalconArray)
 
 
+class CFunction():
+    """
+    A primitive definition of a C Function that, when called, evaluates entirely in C
+
+    FIXME: establish the guanrantee made above
+    """
+
+    def __init__(self, func):
+        self.c_file_builder = CFileBuilder()
+        self.py_func = func
+        self.c_func = None
+
+    def __call__(self, *args):
+        def add_to_files(files):
+            self.c_file_builder.add(files)
+
+        for arg in args:
+            if isinstance(arg, FalconArray):
+                arg._add_to_c_files_for_array = add_to_files
+
+        # TODO: Return the c function INSTEAD!
+        return self.py_func(*args)
+
+
+# This is an example function
+def specialize(func):
+    """
+    Enables the specialization of a function.
+
+    :param: func The function to decorate
+    """
+    new_func = CFunction(func)
+    return new_func
+
+@specialize
+def dcRem(input):  # input has to be one of our objects
+    avg = sum(input) / len(input)
+    return input - avg
+
+
+# This is another example function
+@specialize
+def double_input(input1, input2):
+    diff = input1 - input2
+    print type(diff)
+    return 2 * diff
+
+
 #
 # Testing Code
 #
@@ -290,8 +369,9 @@ if __name__ == '__main__':
     test_inpt1 = FalconArray.array([2.0] * TEST_INPT_LEN)
     test_inpt2 = FalconArray.array([5.0] * TEST_INPT_LEN)
 
-    print "Test Output: ", dcRem(test_inpt1)
+    print "Test Output: ", double_input(test_inpt1, test_inpt2)
 
+    print len(double_input.c_file_builder._c_files)
 
     # print "Addition Result: ", test_inpt1 + test_inpt2
     # print "Subtraction Result: ", test_inpt1 - test_inpt2
